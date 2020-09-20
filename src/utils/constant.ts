118b1b9e14d10144d89f9c14c12f2f9ea6a0aa7d
lijiehong <lijiehong@bytedance.com>
2020-09-20 13:22:42 +0800

    chore: docker compose搭建

--- src/utils/constant.ts
+++ src/utils/constant.ts
@@ -1,7 +1,7 @@
 export const jsObj = 'asjkdlals';
 
-export const GET_HOLDINGS_DATA = 'http://localhost:3002/api/getHoldingsData';
-export const GET_STOCK_DATA = 'http://localhost:3002/api/getStockData';
+// export const GET_HOLDINGS_DATA = 'http://localhost:3002/api/getHoldingsData';
+// export const GET_STOCK_DATA = 'http://localhost:3002/api/getStockData';
 
-// export const GET_HOLDINGS_DATA = '/api/getHoldingsData';
-// export const GET_STOCK_DATA = '/api/getStockData';
+export const GET_HOLDINGS_DATA = '/api/getHoldingsData';
+export const GET_STOCK_DATA = '/api/getStockData';
