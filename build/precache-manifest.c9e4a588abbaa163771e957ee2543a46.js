7ea43681a0c6f21607c6e4892ff9d09b26f992a6
CarolSum <905965287@qq.com>
2020-09-21 00:17:27 +0800

    chore: 切换部署分之

---
+++ build/precache-manifest.c9e4a588abbaa163771e957ee2543a46.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "501d4e2ec740414d96125d7aaa55d4c7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "725c69933b04ce70e455",
+    "url": "/static/css/2.04ff9426.chunk.css"
+  },
+  {
+    "revision": "46d79f1dd9c68de6fff6",
+    "url": "/static/css/main.3e340323.chunk.css"
+  },
+  {
+    "revision": "725c69933b04ce70e455",
+    "url": "/static/js/2.c434a898.chunk.js"
+  },
+  {
+    "revision": "4176373d3a873469c3467aa9e69f6d75",
+    "url": "/static/js/2.c434a898.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "46d79f1dd9c68de6fff6",
+    "url": "/static/js/main.79993389.chunk.js"
+  },
+  {
+    "revision": "b90bec7dc3722f37e569",
+    "url": "/static/js/runtime-main.26e60db2.js"
+  }
+]);
